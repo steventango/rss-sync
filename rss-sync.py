f6d8883e6a2139fd2daaf05f06fca7bf0ce604fd
Steven Tang <18170455+steventango@users.noreply.github.com>
2022-10-24 10:39:29 -0600

    Implement retry logic

--- rss-sync.py
+++ rss-sync.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 import urllib.parse
 import urllib.request
 from datetime import datetime
@@ -7,14 +8,22 @@ from datetime import datetime
 import lxml.etree as ET
 
 
-def rss_sync(url):
+def rss_sync(url, max_retries):
     path = urllib.parse.urlparse(url).path
     basename = os.path.basename(path)
     if not basename.endswith('.rss'):
         basename += '.rss'
 
-    request = urllib.request.urlopen(url)
-    data = request.read()
+    retries = 0
+    while retries < max_retries:
+        try:
+            request = urllib.request.urlopen(url)
+            data = request.read()
+            break
+        except urllib.error.URLError:
+            retries += 1
+            time.sleep(2 ** retries)
+            return
 
     parser = ET.XMLParser(encoding='utf-8', strip_cdata=False)
     patch = ET.fromstring(data, parser)
@@ -26,13 +35,19 @@ def rss_sync(url):
             f.write(data)
             return
 
-    root.find('./channel/lastBuildDate').text = patch.find('./channel/lastBuildDate').text
+    root.find('./channel/lastBuildDate').text = patch.find(
+        './channel/lastBuildDate'
+    ).text
 
     latest_item = root.find('./channel/item')
-    latest_pubDate = datetime.strptime(latest_item.find('./pubDate').text, '%a, %d %b %Y %H:%M:%S %z')
+    latest_pubDate = datetime.strptime(
+        latest_item.find('./pubDate').text, '%a, %d %b %Y %H:%M:%S %z'
+    )
     modified = False
     for item in patch.findall('./channel/item'):
-        pubDate = datetime.strptime(item.find('./pubDate').text, '%a, %d %b %Y %H:%M:%S %z')
+        pubDate = datetime.strptime(
+            item.find('./pubDate').text, '%a, %d %b %Y %H:%M:%S %z'
+        )
         if latest_pubDate >= pubDate:
             break
         latest_item.addprevious(item)
@@ -44,8 +59,11 @@ def rss_sync(url):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('url', help='RSS feed URL')
+    parser.add_argument(
+        '--max_retries', type=int, help='Maximum number of retries', default=11
+        )
     args = parser.parse_args()
-    rss_sync(args.url)
+    rss_sync(args.url, args.max_retries)
 
 
 if __name__ == '__main__':
